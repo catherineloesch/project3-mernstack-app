2dca66d2ca6668dc3488c415a88f79c881c92d42
Catherine Loesch <catherine.loesch@protonmail.com>
2023-06-13 02:09:23 +0100

    readme updated from team document to personal github account

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -8,11 +8,9 @@ import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
 );
 
 
